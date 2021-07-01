e45806c544f9840f4409c970b76a46b61ae67d5e
Srevin Saju <srevinsaju@sugarlabs.org>
2021-07-02 00:21:58 +0300

    style+feat: lint and restyle login and register pages

---
+++ app/src/main/java/in/srev/lyrix/SimilarSongs.kt
@@ -0,0 +1,11 @@
+package `in`.srev.lyrix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimilarSongs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_similar_songs)
+    }
+}
